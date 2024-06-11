b536c9a873d901b735d736495d4d473575b62143
abhishekcgupta1 <abhishekcgupta1@gmail.com>
2024-06-12 00:12:56 +0530

    inital commit

---
+++ PrintMethod.js
@@ -0,0 +1,20 @@
+console.log("Hello World"); 
+
+// - Variable CRUD to store in memory , Named storage to keep data 
+
+
+var firstName = 'abhishek';
+console.log(firstName);
+
+let secondName = 'Gupta';
+secondName='Gupta2'
+console.log(secondName);
+
+// Reserve words
+// CamelCasing - This is the prefrence 
+
+// multi variable on the sameline
+
+let firstName2 = 'Hello', secondName = 'hello1'
+
+// let [Mutable] vs const [Non Mutable] 
